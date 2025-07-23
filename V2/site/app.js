63e9cc68c2fec7ca5008ed78ace4dd238c5d3224
trplRsham <rrashmrr@gmail.com>
2025-07-23 12:02:24 -0700

    add data base - mongoDB through mongoose

--- V2/site/app.js
+++ V2/site/app.js
@@ -1,3 +1,4 @@
+
 function read() {
     const req = new XMLHttpRequest();
     req.open("GET", "http://localhost:3000");
@@ -8,13 +9,13 @@ function read() {
             const objects = JSON.parse(this.responseText);
             let newRow = '';
             for (object of objects) {
-                // console.log(object.id);
-                newRow += `<tr id="${object.id}">`;
+                // console.log(typeof(object._id));
+                newRow += `<tr id="${object._id}">`;
                 newRow += `<td> ${object.name} </td>`;
                 newRow += `<td> ${object.uni} </td>`;
                 newRow += `<td> ${object.field} </td>`;
-                newRow += `<td><button onclick="showEdit(${object.id})">Edit</button></td>`;
-                newRow += `<td><button onclick="deleteRow(${object.id})">Delete</button></td>`;
+                newRow += `<td><button onclick="showEdit('${object._id}')">Edit</button></td>`;
+                newRow += `<td><button onclick="deleteRow('${object._id}')">Delete</button></td>`;
                 newRow += `</tr>`;
             }
             const table = document.getElementById("myTable").innerHTML = newRow;
@@ -28,6 +29,7 @@ function clearForm(form) {
         input.value = "";
     }
 }
+
 
 const form = document.querySelector("form");
 function create() {
@@ -69,35 +71,42 @@ form.addEventListener("submit", e => {
 })
 
 function showEdit(id) {
+    // const id = obj._id;
+    // console.log(id);
+    console.log(id);
     const req = new XMLHttpRequest();
     req.open("GET", `http://localhost:3000/${id}`);
     req.send()
     req.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            // console.log(this.responseText);
+            console.log(this.responseText);
             const object = JSON.parse(this.responseText);
-            // console.log("the object is", object);
-            const nameInput = document.querySelector("#edit_form #name");
-            const uniInput = document.querySelector("#edit_form #uni");
-            const fieldInput = document.querySelector("#edit_form #field");
+            console.log("the object is", object);
+            const id = document.querySelector("#id");
+            const nameInput = document.querySelector("#new_name");
+            const uniInput = document.querySelector("#new_uni");
+            const fieldInput = document.querySelector("#new_field");
             const editButton = document.querySelector("#edit_form button")
 
+            // console.log(nameInput);
+            
             if (nameInput.hasAttribute("hidden")) {
-                nameInput.value = `${object.name}`;
-                uniInput.value = `${object.uni}`;
-                fieldInput.value = `${object.field}`;
+                id.value = `${object[0]._id}`;
+                nameInput.value = `${object[0].name}`;
+                uniInput.value = `${object[0].uni}`;
+                fieldInput.value = `${object[0].field}`;
                 nameInput.removeAttribute("hidden");
                 uniInput.removeAttribute("hidden");
                 fieldInput.removeAttribute("hidden");
                 editButton.removeAttribute("hidden");
-                editButton.addEventListener("click", () => {
+                editButton.onclick = () => {
                     nameInput.setAttribute("hidden", "");
                     uniInput.setAttribute("hidden", "");
                     fieldInput.setAttribute("hidden", "");
                     editButton.setAttribute("hidden", "");
-
-                    edit(id, nameInput.value, uniInput.value, fieldInput.value);
-                });
+                    console.log(id.value, nameInput.value, uniInput.value, fieldInput.value);
+                    edit(id.value, nameInput.value, uniInput.value, fieldInput.value);
+                }
             } else {
                 alert("first submit current changes!");
 

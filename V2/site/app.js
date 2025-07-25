f8d7e4e9d82f4a1f575896182d5c67a6f937c940
trplRsham <rrashmrr@gmail.com>
2025-07-24 21:31:52 -0700

    add filter on data table

--- V2/site/app.js
+++ V2/site/app.js
@@ -1,4 +1,19 @@
 
+function showData(objects) {
+    let newRow = '';
+    for (object of objects) {
+        // console.log(typeof(object._id));
+        newRow += `<tr id="${object._id}">`;
+        newRow += `<td> ${object.name} </td>`;
+        newRow += `<td> ${object.uni} </td>`;
+        newRow += `<td> ${object.field} </td>`;
+        newRow += `<td><div class="btn-group"><button class="btn btn-info" onclick="showEdit('${object._id}')">Edit</button>`;
+        newRow += `<button class="btn btn-danger" onclick="deleteRow('${object._id}')">Delete</button></div></td>`;
+        newRow += `</tr>`;
+    }
+    const table = document.getElementById("myTable").innerHTML = newRow;
+}
+
 function read() {
     const req = new XMLHttpRequest();
     req.open("GET", "http://localhost:8080");
@@ -7,22 +22,11 @@ function read() {
         if (this.readyState == 4 && this.status == 200) {
             // console.log(this.responseText);
             const objects = JSON.parse(this.responseText);
-            let newRow = '';
-            for (object of objects) {
-                // console.log(typeof(object._id));
-                newRow += `<tr id="${object._id}">`;
-                newRow += `<td> ${object.name} </td>`;
-                newRow += `<td> ${object.uni} </td>`;
-                newRow += `<td> ${object.field} </td>`;
-                newRow += `<td><div class="btn-group"><button class="btn btn-info" onclick="showEdit('${object._id}')">Edit</button>`;
-                newRow += `<button class="btn btn-danger" onclick="deleteRow('${object._id}')">Delete</button></div></td>`;
-                newRow += `</tr>`;
-            }
-            const table = document.getElementById("myTable").innerHTML = newRow;
+            showData(objects)
         }
     }
 }
-read();
+// read();
 
 function clearForm(form) {
     for (input of form) {
@@ -171,4 +175,62 @@ function deleteRow(id) {
             read();
         }
     }
+}
+
+function chackValidation(fieldList) {
+    for (field of fieldList) {
+        if (field.classList.contains("is-invalid")) {
+            field.classList.remove("is-invalid");
+        }
+
+        if (!field.value || field.value == "NULL") {
+            alert("fill the fields");
+            field.classList.add("is-invalid");
+            return false;
+        }
+    }
+    return true;
+}
+
+const filterBtn = document.querySelector("#filter_button");
+const removeFilterBtn = document.querySelector("#remove_filter_button");
+function filter() {
+    const filterOpt = document.querySelector("#filter_on");
+    const search = document.querySelector("#search");
+    // console.dir(filterBtn);
+    if (!chackValidation([filterOpt, search])) return;
+
+    const filterOptValue = filterOpt.value;
+    const searchValue = search.value;
+    const tagsArr = searchValue.split(",");
+    console.log(tagsArr);
+    let queryString = "";
+    for (tag of tagsArr){
+        queryString += `values=${tag}&`;
+    }
+    // console.log(queryString);
+
+    const req = new XMLHttpRequest();
+    req.open("GET", `http://localhost:8080?${queryString}key=${filterOptValue}`);
+    req.send();
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const objects = JSON.parse(this.responseText);
+            showData(objects)
+        }
+    }
+}
+
+filterBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    filter();
+})
+removeFilterBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    clearForm(document.querySelector("#filter_form"));
+    read();
+})
+
+window.onload = () => {
+    read();
 }
